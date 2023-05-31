1f94e1a38be5e9fd35841db84d63819f2d7f213b
ahava <minoraherinirina72@gmail.com>
2023-05-31 09:51:01 +0200

    Login

---
+++ resources/views/BO/all.blade.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>All</title>
+    </head>
+    <body>
+    <h1>Liste des comptes Back Office</h1>
+
+        <ul>
+            @foreach ($all as $account)
+                <li>{{ $account->firstname }} {{ $account->name }}</li>
+            @endforeach
+        </ul>
+    
+
+    </body>
+</html>
